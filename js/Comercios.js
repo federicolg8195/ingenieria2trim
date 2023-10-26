c244b44ffdb89354842249ee215feb597983d708
federicolg8195 <federico.l.g8195@gmail.com>
2023-10-26 20:37:43 -0300

    fds

---
+++ js/Comercios.js
@@ -0,0 +1,31 @@
+/*Para cada comercio se mostrará el nombre y una descripción general del
+negocio, las categorías relacionadas, y los datos de contacto como la dirección,
+horarios, teléfono, alguna foto principal, etc.*/
+
+
+let Comercios = {
+    data:[
+        {   
+            nombre:"Centro + Vida",
+            categoria:"CentroDeApoyo",
+            descripcion: "Amazonas 398", 
+            horario:"de 8hs a 12hs y de 17hs a 22hs",
+            telefono:"1138993411",
+            image:"img/centro medico.jpg",
+            
+
+        },
+        {
+          nombre:"Centro Salud",
+          categoria:"CentroDeApoyo",
+          descripcion: "Cerro Catedral 550", 
+          horario:"24 HS",
+          telefono:"1138968595",
+          image:"img/centro medico.jpg",
+            
+        },
+        
+       
+    ]
+};
+
